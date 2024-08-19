f62402cca7e2e7136b3acb6c098187ff24c6d6e7
DummyCoder212 <jason@spotdraft.com>
2024-08-19 22:01:41 +0530

    changed CORS logic

--- server/server.js
+++ server/server.js
@@ -11,27 +11,19 @@ const fileRoutes = require("./routes/file");
 
 const app = express();
 
-// Middleware
 app.use(express.json());
 
 const allowedOrigin = process.env.ALLOWED_ORIGIN;
 
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (origin === allowedOrigin || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: allowedOrigin,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
 app.use(methodOverride("_method"));
 
-// Use routes
 app.use("/api/user", userRoutes);
 app.use("/api/contracts", contractRoutes);
 
@@ -40,10 +32,8 @@ app.use("/api/files", fileRoutes);
 mongoose
   .connect(process.env.MONGO_URI)
   .then((conn) => {
-    // Initialize GridFS
     init(conn.connection.db);
 
-    // Start server
     app.listen(process.env.PORT, () => {
       console.log("Connected to DB & listening on port", process.env.PORT);
     });
